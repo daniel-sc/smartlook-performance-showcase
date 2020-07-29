0f5b7c15395ecba14a41eb09edb9b5dad14ede30
e502692 <daniel.schreiber@sbb.ch>
2020-07-29 07:41:27 +0200

    minimal example for slow smartlook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import smartlookClient from 'smartlook-client'
+import {FormControl} from '@angular/forms';
+import {distinctUntilChanged} from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <img width="300" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+    selector: 'app-root',
+    template: `
+        <h1>Performance Showcase Smartlook</h1>
+        <label style="display: block">
+            Enable Smartlook
+            <input type="checkbox" [formControl]="enableFormControl">
+        </label>
+
+        <button (click)="rows = []; duration = null">Clear</button>
+        <button (click)="generateRows()">Generate</button>
+        <div>Duration: {{duration}}ms</div>
+
+        <div>
+            <h2>Rows</h2>
+            <div *ngFor="let row of rows">
+                <span>{{row}}</span>
+            </div>
+        </div>
+    `,
+    styles: []
 })
-export class AppComponent {
-  title = 'smartlook-performance-showcase';
+export class AppComponent implements OnInit {
+
+    rows = [];
+    duration: number | null = null;
+
+    readonly enableFormControl = new FormControl(true);
+
+    ngOnInit() {
+        smartlookClient.init('f6da96c5947b7de535697e0a8dfa0d1759f18bca');
+        smartlookClient.consentAPI('Akzeptiert gemäss DSE und Einstellungen im Benutzerprofil');
+        smartlookClient.consentForms('Akzeptiert gemäss DSE und Einstellungen im Benutzerprofil');
+
+        this.enableFormControl.valueChanges.pipe(distinctUntilChanged()).subscribe((enabled: boolean) => {
+            if (enabled) {
+                smartlookClient.resume();
+            } else {
+                smartlookClient.pause();
+            }
+        })
+    }
+
+    generateRows() {
+        const start = Date.now();
+        for (let i = 0; i < 100; i++) {
+            this.rows.push(`hello ${i}`)
+        }
+        //measure time after paint:
+        setTimeout(() => this.duration = Date.now() - start);
+    }
 }
